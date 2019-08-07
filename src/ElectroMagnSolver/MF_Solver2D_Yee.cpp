f1d444d2636e777ed2638d884ad567384e5c20a2
pederson <dpederson@utexas.edu>
2019-08-07 13:10:43 -0500

    (debug) of an unmatched if/else in the Yee solver with magnetic current imposed

--- src/ElectroMagnSolver/MF_Solver2D_Yee.cpp
+++ src/ElectroMagnSolver/MF_Solver2D_Yee.cpp
@@ -41,67 +41,69 @@ void MF_Solver2D_Yee::operator()( ElectroMagn *fields )
 
     if (Jmx2D && Jmy2D && Jmz2D){
         {
-        #pragma omp simd
-        for( unsigned int j=1 ; j<ny_d-1 ; j++ ) {
-            ( *Bx2D )( 0, j ) += -dt*( *Jmx2D )( 0, j ) - dt_ov_dy * ( ( *Ez2D )( 0, j ) - ( *Ez2D )( 0, j-1 ) );
+            #pragma omp simd
+            for( unsigned int j=1 ; j<ny_d-1 ; j++ ) {
+                ( *Bx2D )( 0, j ) += -dt*( *Jmx2D )( 0, j ) - dt_ov_dy * ( ( *Ez2D )( 0, j ) - ( *Ez2D )( 0, j-1 ) );
+            }
         }
-    }
-    //    for (unsigned int i=0 ; i<nx_p;  i++) {
-    for( unsigned int i=1 ; i<nx_d-1;  i++ ) {
-        #pragma omp simd
-        for( unsigned int j=1 ; j<ny_d-1 ; j++ ) {
-            ( *Bx2D )( i, j ) += -dt*( *Jmx2D )( i, j ) - dt_ov_dy * ( ( *Ez2D )( i, j ) - ( *Ez2D )( i, j-1 ) );
+        //    for (unsigned int i=0 ; i<nx_p;  i++) {
+        for( unsigned int i=1 ; i<nx_d-1;  i++ ) {
+            #pragma omp simd
+            for( unsigned int j=1 ; j<ny_d-1 ; j++ ) {
+                ( *Bx2D )( i, j ) += -dt*( *Jmx2D )( i, j ) - dt_ov_dy * ( ( *Ez2D )( i, j ) - ( *Ez2D )( i, j-1 ) );
+            }
+            //    }
+            
+            // Magnetic field By^(d,p)
+            //    for (unsigned int i=1 ; i<nx_d-1 ; i++) {
+            #pragma omp simd
+            for( unsigned int j=0 ; j<ny_p ; j++ ) {
+                ( *By2D )( i, j ) += -dt*( *Jmy2D )( i, j ) + dt_ov_dx * ( ( *Ez2D )( i, j ) - ( *Ez2D )( i-1, j ) );
+            }
+            //}
+            
+            // Magnetic field Bz^(d,d)
+            //for (unsigned int i=1 ; i<nx_d-1 ; i++) {
+            #pragma omp simd
+            for( unsigned int j=1 ; j<ny_d-1 ; j++ ) {
+                ( *Bz2D )( i, j ) += -dt*( *Jmz2D )( i, j ) + dt_ov_dy * ( ( *Ex2D )( i, j ) - ( *Ex2D )( i, j-1 ) )
+                                     -               dt_ov_dx * ( ( *Ey2D )( i, j ) - ( *Ey2D )( i-1, j ) );
+            }
         }
-        //    }
-        
-        // Magnetic field By^(d,p)
-        //    for (unsigned int i=1 ; i<nx_d-1 ; i++) {
-        #pragma omp simd
-        for( unsigned int j=0 ; j<ny_p ; j++ ) {
-            ( *By2D )( i, j ) += -dt*( *Jmy2D )( i, j ) + dt_ov_dx * ( ( *Ez2D )( i, j ) - ( *Ez2D )( i-1, j ) );
-        }
-        //}
-        
-        // Magnetic field Bz^(d,d)
-        //for (unsigned int i=1 ; i<nx_d-1 ; i++) {
-        #pragma omp simd
-        for( unsigned int j=1 ; j<ny_d-1 ; j++ ) {
-            ( *Bz2D )( i, j ) += -dt*( *Jmz2D )( i, j ) + dt_ov_dy * ( ( *Ex2D )( i, j ) - ( *Ex2D )( i, j-1 ) )
-                                 -               dt_ov_dx * ( ( *Ey2D )( i, j ) - ( *Ey2D )( i-1, j ) );
-        }
-    }
     }
     // Magnetic field Bx^(p,d)
     //cout << "nx_p,nx_d-1" << nx_p << " " << nx_d-1 ;
     //{
-    {
-        #pragma omp simd
-        for( unsigned int j=1 ; j<ny_d-1 ; j++ ) {
-            ( *Bx2D )( 0, j ) -= dt_ov_dy * ( ( *Ez2D )( 0, j ) - ( *Ez2D )( 0, j-1 ) );
+    else{
+        {
+            #pragma omp simd
+            for( unsigned int j=1 ; j<ny_d-1 ; j++ ) {
+                ( *Bx2D )( 0, j ) -= dt_ov_dy * ( ( *Ez2D )( 0, j ) - ( *Ez2D )( 0, j-1 ) );
+            }
         }
-    }
-    //    for (unsigned int i=0 ; i<nx_p;  i++) {
-    for( unsigned int i=1 ; i<nx_d-1;  i++ ) {
-        #pragma omp simd
-        for( unsigned int j=1 ; j<ny_d-1 ; j++ ) {
-            ( *Bx2D )( i, j ) -= dt_ov_dy * ( ( *Ez2D )( i, j ) - ( *Ez2D )( i, j-1 ) );
-        }
-        //    }
-        
-        // Magnetic field By^(d,p)
-        //    for (unsigned int i=1 ; i<nx_d-1 ; i++) {
-        #pragma omp simd
-        for( unsigned int j=0 ; j<ny_p ; j++ ) {
-            ( *By2D )( i, j ) += dt_ov_dx * ( ( *Ez2D )( i, j ) - ( *Ez2D )( i-1, j ) );
-        }
-        //}
-        
-        // Magnetic field Bz^(d,d)
-        //for (unsigned int i=1 ; i<nx_d-1 ; i++) {
-        #pragma omp simd
-        for( unsigned int j=1 ; j<ny_d-1 ; j++ ) {
-            ( *Bz2D )( i, j ) += dt_ov_dy * ( ( *Ex2D )( i, j ) - ( *Ex2D )( i, j-1 ) )
-                                 -               dt_ov_dx * ( ( *Ey2D )( i, j ) - ( *Ey2D )( i-1, j ) );
+        //    for (unsigned int i=0 ; i<nx_p;  i++) {
+        for( unsigned int i=1 ; i<nx_d-1;  i++ ) {
+            #pragma omp simd
+            for( unsigned int j=1 ; j<ny_d-1 ; j++ ) {
+                ( *Bx2D )( i, j ) -= dt_ov_dy * ( ( *Ez2D )( i, j ) - ( *Ez2D )( i, j-1 ) );
+            }
+            //    }
+            
+            // Magnetic field By^(d,p)
+            //    for (unsigned int i=1 ; i<nx_d-1 ; i++) {
+            #pragma omp simd
+            for( unsigned int j=0 ; j<ny_p ; j++ ) {
+                ( *By2D )( i, j ) += dt_ov_dx * ( ( *Ez2D )( i, j ) - ( *Ez2D )( i-1, j ) );
+            }
+            //}
+            
+            // Magnetic field Bz^(d,d)
+            //for (unsigned int i=1 ; i<nx_d-1 ; i++) {
+            #pragma omp simd
+            for( unsigned int j=1 ; j<ny_d-1 ; j++ ) {
+                ( *Bz2D )( i, j ) += dt_ov_dy * ( ( *Ex2D )( i, j ) - ( *Ex2D )( i, j-1 ) )
+                                     -               dt_ov_dx * ( ( *Ey2D )( i, j ) - ( *Ey2D )( i-1, j ) );
+            }
         }
     }
     //}// end parallel
